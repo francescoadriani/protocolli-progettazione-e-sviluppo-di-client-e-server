e79e3da0d7e3d7f6860abe229ba5d80c0914e6e0
francescoadriani <90975508+francescoadriani@users.noreply.github.com>
2021-11-09 09:56:03 +0100

    Creato nuovo protocollo, inserito documento del protocollo

---
+++ Protocollo RANDOMIZZATORE/Server/RandomizzatoreServer/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace RandomizzatoreServer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
