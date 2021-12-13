f904f1f3ff793aa2c6b224526f20d4b25eb1e0af
francescoadriani <90975508+francescoadriani@users.noreply.github.com>
2021-12-13 23:17:37 +0100

    ripristinato esempio come in origine (ECHO PROGRAM)

--- Protocollo RANDOMIZZATORE esp/Server/esempio_server_web/esempio_server_web.ino
+++ Protocollo RANDOMIZZATORE esp/Server/esempio_server_web/esempio_server_web.ino
@@ -1,9 +1,8 @@
 #include <ESP8266WiFi.h> // #include <WiFi.h>
-const char* ssid = "ASUS_RiceWLan";
-const char* password = "pippoplutopaperinominnie";
+const char* ssid = "nomewifi";
+const char* password = "pwswifi";
 WiFiServer serverTcp(10103);
 void setup() {
-  randomSeed(analogRead(0));
   Serial.begin(115200);
   WiFi.begin(ssid,password);
   while(WiFi.status() != WL_CONNECTED ){ delay(500); }
@@ -16,52 +15,20 @@ void loop() {
   WiFiClient clientTcp = serverTcp.available();
   if (!clientTcp) { return; }
   Serial.println("New client connected");
-  double min=0;
-  double max=1.0;
   while (true)
   {
-    String answer = "";
     while(!clientTcp.available()){ delay(1); }
     String message = clientTcp.readStringUntil('\r');
     message.replace("\n","");
     message.replace("\r","");
     Serial.println(message);
+    clientTcp.print("ECHO:");
+    clientTcp.println(message);
     delay(1);
-    if (message.indexOf("GET")>-1)
-    {
-      if (message.indexOf("GET[")>-1)
-      {
-        int indexOfSeparator = message.indexOf(";");
-        int indexOfSecondPare = message.indexOf("]");
-        if (indexOfSeparator>-1 && indexOfSecondPare>-1)
-        {
-          Serial.print("New min: ");
-          Serial.println(message.substring(4,indexOfSeparator));
-          Serial.print("New max: ");
-          Serial.println(message.substring(indexOfSeparator+1,indexOfSecondPare));
-          min = message.substring(4,indexOfSeparator).toDouble();
-          max = message.substring(indexOfSeparator+1,indexOfSecondPare).toDouble();
-        }
-      }
-      Serial.print("min: ");
-      Serial.println(String(min));
-      Serial.print("max: ");
-      Serial.println(String(max));
-      double rnd = random(1000000);
-      Serial.print("rnd: ");
-      Serial.println(String(rnd));
-      double rndMinMax = min + (rnd * (max-min) /1000000.0);
-      Serial.print("rndMinMax: ");
-      Serial.println(String(rndMinMax));
-      answer = String(rndMinMax);
-    }
-    else if (message.indexOf("CLOSE")>-1)
+    if (message.indexOf("CLOSE")>-1)
     {
         Serial.println("Client requested disconnection");
         clientTcp.stop();
     }
-    else
-      answer = "COMMAND UNKNOW";
-    clientTcp.println(answer);
   }
 }
