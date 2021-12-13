86a390f57d8c6d55891066f229927063c51d4643
francescoadriani <90975508+francescoadriani@users.noreply.github.com>
2021-12-13 22:07:25 +0100

    aggiornato il programma togliendo tutti gli acapo dai messaggi e rimettendoli dove bisogna

--- Protocollo RANDOMIZZATORE esp/Server/esempio_server_web/esempio_server_web.ino
+++ Protocollo RANDOMIZZATORE esp/Server/esempio_server_web/esempio_server_web.ino
@@ -9,18 +9,20 @@ void setup() {
   Serial.print("Wifi Connected Success!, IP Address: ");
   Serial.println(WiFi.localIP() );
   serverTcp.begin();
+  Serial.println("Server started on port 10103");
 }
 void loop() {
   WiFiClient clientTcp = serverTcp.available();
   if (!clientTcp) { return; }
+  Serial.println("New client connected");
   while (true)
   {
     while(!clientTcp.available()){ delay(1); }
     String message = clientTcp.readStringUntil('\r');
+    message.replace("\n","");
     Serial.println(message);
     clientTcp.print("ECHO:");
     clientTcp.println(message);
-    clientTcp.println();
     delay(1);
     if (message.indexOf("CLOSE")>-1)
     {
