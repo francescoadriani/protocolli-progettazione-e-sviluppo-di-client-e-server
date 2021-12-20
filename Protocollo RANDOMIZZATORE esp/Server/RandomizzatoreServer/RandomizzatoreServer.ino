5f3e01e192c501755a0b9827cf8283c15242cf3c
rice103 <rice103@gmail.com>
2021-12-20 01:25:14 +0100

    aggiornati i programmi con l'utilizzo del led

--- Protocollo RANDOMIZZATORE esp/Server/RandomizzatoreServer/RandomizzatoreServer.ino
+++ Protocollo RANDOMIZZATORE esp/Server/RandomizzatoreServer/RandomizzatoreServer.ino
@@ -1,12 +1,14 @@
-#include <ESP8266WiFi.h> // #include <WiFi.h>
+#include <WiFi.h> // #include <ESP8266WiFi.h> // 
+#define LED_BUILTIN 2
 const char* ssid = "esp8266_AP";
 const char* password = "12345678";
 WiFiServer serverTcp(10103);
 void setup() {
+  pinMode(LED_BUILTIN, OUTPUT);
   randomSeed(analogRead(0));
-  Serial.begin(115200);
+  Serial.begin(9600);
   WiFi.mode(WIFI_AP); // attivazione della wifi come client wifi
-  WiFi.softAP(ssid, password); // attiva la modalità Access Point 
+  WiFi.softAP(ssid, password); // attiva la modalità Access Point
   Serial.println();
   Serial.print("MAC Address: ");
   Serial.println(WiFi.macAddress());
@@ -16,49 +18,60 @@ void setup() {
   Serial.println(password);
   Serial.print("AP ip address: ");
   Serial.println(WiFi.softAPIP());
-  
+
   //WiFi.begin(ssid,password);
   //while(WiFi.status() != WL_CONNECTED ){ delay(500); }
   //Serial.print("Wifi Connected Success!, IP Address: ");
   //Serial.println(WiFi.localIP() );
   serverTcp.begin();
   Serial.println("Server started on port 10103");
+  digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on (HIGH is the voltage level)
 }
 void loop() {
   WiFiClient clientTcp = serverTcp.available();
-  if (!clientTcp) { return; }
+  digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on (HIGH is the voltage level)
+  if (!clientTcp) {
+    return;
+  }
   Serial.println("New client connected");
-  double min=0;
-  double max=1.0;
+  digitalWrite(LED_BUILTIN, LOW);    // turn the LED off by making the voltage LOW
+  double min = 0;
+  double max = 1.0;
   while (true)
   {
     String answer = "";
-    while(!clientTcp.available()){ delay(1); }
+    while (!clientTcp.available()) {
+      delay(1);
+    }
     String message = clientTcp.readStringUntil('\r');
-    message.replace("\n","");
-    message.replace("\r","");
+    digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on (HIGH is the voltage level)
+    delay(100);                       // wait for a second
+    digitalWrite(LED_BUILTIN, LOW);    // turn the LED off by making the voltage LOW
+    message.replace("\n", "");
+    message.replace("\r", "");
     Serial.println(message);
     delay(1);
-    if (message.indexOf("GET")>-1)
+    if (message.indexOf("GET") > -1)
     {
-      if (message.indexOf("GET[")>-1)
+      if (message.indexOf("GET[") > -1)
       {
         int indexOfSeparator = message.indexOf(";");
         int indexOfSecondPare = message.indexOf("]");
-        if (indexOfSeparator>-1 && indexOfSecondPare>-1)
+        if (indexOfSeparator > -1 && indexOfSecondPare > -1)
         {
-          min = message.substring(4,indexOfSeparator).toDouble();
-          max = message.substring(indexOfSeparator+1,indexOfSecondPare).toDouble();
+          min = message.substring(4, indexOfSeparator).toDouble();
+          max = message.substring(indexOfSeparator + 1, indexOfSecondPare).toDouble();
         }
       }
       double rnd = random(1000000);
-      double rndMinMax = min + (rnd * (max-min) /1000000.0);
+      double rndMinMax = min + (rnd * (max - min) / 1000000.0);
       answer = String(rndMinMax);
     }
-    else if (message.indexOf("CLOSE")>-1)
+    else if (message.indexOf("CLOSE") > -1)
     {
-        Serial.println("Client requested disconnection");
-        clientTcp.stop();
+      Serial.println("Client requested disconnection");
+      clientTcp.stop();
+      return;
     }
     else
       answer = "COMMAND UNKNOW";
--- Protocollo RANDOMIZZATORE esp/Server/esempio_server_web/esempio_server_web.ino
+++ Protocollo RANDOMIZZATORE esp/Server/esempio_server_web/esempio_server_web.ino
@@ -1,24 +1,35 @@
 #include <ESP8266WiFi.h> // #include <WiFi.h>
+#define LED_BUILTIN 2
 const char* ssid = "nomewifi";
 const char* password = "pwswifi";
 WiFiServer serverTcp(10103);
 void setup() {
+  pinMode(LED_BUILTIN, OUTPUT);
+  digitalWrite(LED_BUILTIN, LOW);   // turn the LED on (HIGH is the voltage level)
   Serial.begin(115200);
   WiFi.begin(ssid,password);
   while(WiFi.status() != WL_CONNECTED ){ delay(500); }
-  Serial.print("Wifi Connected Success!, IP Address: ");
+  Serial.print("Wifi Connected Success!\n\rIP Address: ");
   Serial.println(WiFi.localIP() );
+  Serial.print("MAC Address: ");
+  Serial.println(WiFi.macAddress());
   serverTcp.begin();
   Serial.println("Server started on port 10103");
+  digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on (HIGH is the voltage level)
 }
 void loop() {
   WiFiClient clientTcp = serverTcp.available();
+  digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on (HIGH is the voltage level)
   if (!clientTcp) { return; }
   Serial.println("New client connected");
+  digitalWrite(LED_BUILTIN, LOW);    // turn the LED off by making the voltage LOW
   while (true)
   {
     while(!clientTcp.available()){ delay(1); }
     String message = clientTcp.readStringUntil('\r');
+    digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on (HIGH is the voltage level)
+    delay(100);                       // wait for a second
+    digitalWrite(LED_BUILTIN, LOW);    // turn the LED off by making the voltage LOW
     message.replace("\n","");
     message.replace("\r","");
     Serial.println(message);
